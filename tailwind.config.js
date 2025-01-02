1233c5100ea62ff73a9b6c51fb1fad018df612f6
paramientos <paramientos>
2025-01-02 10:54:30 +0000

    Pushed from Locofy Builder

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}", "./!(build|dist|.*)/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          "100": "#242424",
+          "200": "rgba(28, 27, 41, 0.4)",
+          "300": "rgba(28, 27, 41, 0.21)",
+        },
+        white: "#fff",
+        "button-normal": "#ff5733",
+        black: "#000",
+      },
+      spacing: {},
+      fontFamily: {
+        vazirmatn: "Vazirmatn",
+        viga: "Viga",
+      },
+      borderRadius: {
+        xl: "20px",
+        "21xl": "40px",
+      },
+    },
+    fontSize: {
+      lg: "18px",
+      base: "16px",
+      xl: "20px",
+      "13xl": "32px",
+      inherit: "inherit",
+    },
+    screens: {
+      lg: {
+        max: "1200px",
+      },
+      mq1050: {
+        raw: "screen and (max-width: 1050px)",
+      },
+      mq750: {
+        raw: "screen and (max-width: 750px)",
+      },
+      mq450: {
+        raw: "screen and (max-width: 450px)",
+      },
+    },
+  },
+  corePlugins: {
+    preflight: false,
+  },
+};
